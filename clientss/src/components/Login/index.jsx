1cd0ccce8c4871ecf11b7fbf4b55ec81f5cd872b
velan <velanvicky01@gmail.com>
2022-04-23 11:21:07 +0530

    1

--- clientss/src/components/Login/index.jsx
+++ clientss/src/components/Login/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import styles from "./styles.module.css";
 
 const Login = () => {
