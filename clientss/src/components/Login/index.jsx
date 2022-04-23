a77976b172a0df183b9598068635995a8cf1252f
velan <velanvicky01@gmail.com>
2022-04-23 10:38:21 +0530

    1

--- clientss/src/components/Login/index.jsx
+++ clientss/src/components/Login/index.jsx
@@ -14,7 +14,7 @@ const Login = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const url = "http://localhost:5000/api/auth";
+			const url = "https://bookstore-mi.herokuapp.com/api/auth";
 			const { data: res } = await axios.post(url, data);
 			localStorage.setItem("token", res.data);
 			window.location = "/home";
--- clientss/src/components/Singup/index.jsx
+++ clientss/src/components/Singup/index.jsx
@@ -20,7 +20,7 @@ const Signup = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const url = "http://localhost:5000/api/users";
+			const url = "https://bookstore-mi.herokuapp.com/api/users";
 			const { data: res } = await axios.post(url, data);
 			navigate("/login");
 			console.log(res.message);
